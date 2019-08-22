5d9e9c0295c172c9b18535d50160523029a3987d
Matt Whitlock <mattwhitlock@phaeron.com>
2019-08-22 09:56:45 -0600

    Updated Jazzy configuration.

--- gendocs_jazzy.sh
+++ gendocs_jazzy.sh
@@ -1,7 +1,9 @@
 cd Source
 jazzy --clean \
+  --author "Matt Whitlock" \
   --exclude=Carthage/*,build/* \
   --min-acl private \
   --output ../Documentation \
   --theme fullwidth \
-  --documentation=../*.md
+  --documentation=../docs/*.md \
+  --readme ../README.md
